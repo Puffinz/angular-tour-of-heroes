8f4404c5cfa789272a3d54fcadf4bcf214475519
Shawn <shawnboshuis@btc.pe.ca>
2019-11-22 16:10:52 -0400

    Completed part 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Bunnyboy' },
+    { id: 12, name: 'Mr Sniffle' },
+    { id: 13, name: 'Pumpquin' },
+    { id: 14, name: 'Snoro' },
+    { id: 15, name: 'Magenta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Bumber' },
+    { id: 18, name: 'Dr Boignon' },
+    { id: 19, name: 'Smello' },
+    { id: 20, name: 'GuineaBee' }
+];
