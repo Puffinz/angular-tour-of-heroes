732082dfd9cc49ee5af260329f7858ef7dba326b
Shawn <shawnboshuis@btc.pe.ca>
2019-11-25 10:51:44 -0400

    Add/Update/Delete hero functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private messageService: MessageService, private http: HttpClient) { }
 
@@ -28,6 +31,36 @@ export class HeroService {
         tap(_ => this.log('fetched hero id=${id}')),
         catchError(this.handleError<Hero>('getHero id=${id}'))
       );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    var id: number;
+    if(typeof hero == 'number') {
+      id = hero;
+    }
+    else {
+      id = hero.id;
+    }
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 
   private log(message: string) {
